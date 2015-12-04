0be00f9152a9c2fd3b6b67662555efb1442ff34b
Khayet <jakob.herpel@uni-weimar.de>
2015-12-04 21:24:50 +0100

    Fixed quicksort for integer-values. ToDo: templatize

--- main.cpp
+++ main.cpp
@@ -20,22 +20,24 @@ void swap(T& lhs, T& rhs) {
   rhs = temp;
 }*/
 
-void swap(int lhs, int rhs) {
-  int temp = lhs;
-  lhs = rhs;
-  rhs = temp;
+void swap(std::vector<int>& v, int lhs, int rhs) {
+  int temp = v[lhs];
+  v[lhs] = v[rhs];
+  v[rhs] = temp;
 }
 
 bool quicksort(std::vector<int>& vec, int start, int end) {
-  if (vec.size() < 1) return true;
-  if (vec.size() == 2) {
-    if (vec[start] > vec[end]) swap(vec[start], vec[end]);
+  int length = end-start+1;
+
+  if (length <= 1) return true;
+  if (length == 2) {
+    if (vec[start] > vec[end]) swap(vec, start, end);
     return true;
   }
-  if (vec.size() == 3) {
-    if (vec[start] > vec[start+1]) swap(vec[start], vec[start+1]);
-    if (vec[start+1] > vec[end]) swap(vec[start+1], vec[end]);
-    if (vec[start] > vec[start+1]) swap(vec[start], vec[start+1]);
+  if (length == 3) {
+    if (vec[start] > vec[start+1]) swap(vec, start, start+1);
+    if (vec[start+1] > vec[end]) swap(vec, start+1, end);
+    if (vec[start] > vec[start+1]) swap(vec, start, start+1);
     return true;
   }
 
@@ -52,11 +54,15 @@ bool quicksort(std::vector<int>& vec, int start, int end) {
       --right;
     }
 
-    swap(vec[left], vec[right]);
+    if (left < right) { 
+      swap(vec, left, right);
+      ++left;
+      --right;
+    }
+
   }
 
-  swap(vec[start], vec[right]);
-
+  swap(vec, start, right);
   quicksort(vec, start, right-1);
   quicksort(vec, right, end);
 
@@ -66,12 +72,13 @@ bool quicksort(std::vector<int>& vec, int start, int end) {
 
 int main(int argc, char* argv[]) {
   std::vector<int> v;
-  
-  for (int i=0; i < argc; ++i) {
-    v.push_back(std::atoi(argv[i]));
+
+  for (int i=1; i < argc; ++i) {
+    v.push_back(std::stoi(argv[i]));
   }
 
-  quicksort(v, 0, v.size());
+  print_vector(v);
+  quicksort(v, 0, v.size()-1);
   print_vector(v);
   
   return 0;
